59d722fc3cca276e31fc0ccde660b381d7267976
unknown <townej@SLP-AA-TOWNEJ.endeavour.sa.edu.au>
2015-08-23 22:52:53 +0930

    Commented the heck out of the html and js
    also made the readme slightly useful

--- Readme.txt
+++ Readme.txt
@@ -1,1 +1,5 @@
-This is a useless file that nobody uses! yay!
+The files:
+	index.html - The main page, with the base info
+	default.css - the default stylesheet
+	mobile.css - the stylesheet that is used for mobile devices to make the site more mobile friendly
+	library.js - a library of javascript functions to make life easier
--- library.js
+++ library.js
@@ -1,3 +1,7 @@
+			/* this function detects if a user is viewing 
+			the website from one of the mobiles listed below, 
+			if so, it runs another function to make the site
+			 more mobile friendly. */
 			function detectMobile() { 
  				if( navigator.userAgent.match(/Android/i)
  					|| navigator.userAgent.match(/webOS/i)
@@ -13,14 +17,26 @@
     				changeStyle("default.css");
   				}
 			}
+			/* This function converts the site to a 
+			more mobile friendly layout by changing 
+			the stylesheet, which changes things 
+			such as text size to make it easier 
+			to read on a small screen. It also 
+			changes the background image and 
+			removes menu items that are unneeded
+			on the mobile site */
 			function convertMobile() {
 				changeStyle("mobile.css");
 				changeImage("bgimg", "Photos/mobileBg.png");
 				document.getElementById("menu").innerHTML = '<ul><li id = "logoContainer"><img id = "logoImage" src ="Photos/apexLogo.fw.png" width = 40px > </li></ul>';
 			}
+			/* this function can be used to change
+			any image on the site to another image
+			assuming that the image has an ID. */
 			function changeImage(id, img) {
 				document.getElementById(id).setAttribute("src", img);
 			}
+			/* this function can be used to change the page's stylesheet */
 			function changeStyle(sheet) {
 				document.getElementById("pageStyle").setAttribute("href", sheet);
 			}
