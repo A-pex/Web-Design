9c035427542b6d4d205bf52d7a64adf93e3aa50e
unknown <TowneJ@SLP-AA-TOWNEJ.endeavour.sa.edu.au>
2015-08-30 21:25:26 +0930

    Changed photos around resized the form Rewrote most of the website Removed and added some scripts resized some elements in the mobile mode

--- js/library.js
+++ js/library.js
@@ -8,33 +8,29 @@
 			on the mobile site */
 			function convertMobile() {
 				changeStyle("mobile.css");
-				changeImage("bgimg", "Photos/mobileBg.png");
-				document.getElementById("menu").innerHTML = '<ul><li id = "logoContainer"><img id = "logoImage" src ="Photos/apexLogo.fw.png" width = 40px onClick="mobileMenu();"></li></ul>';
-				//writeTo("menu", '<ul><li id = "logoContainer"><img id = "logoImage" src ="Photos/apexLogo.fw.png" width = 40px onClick;="mobileMenu()"></li></ul>');
+				changeSrc("bgimg", "Photos/mobileBg.png");
+				writeTo("menu", '<ul><li id = "logoContainer"><img id = "logoImage" src ="Photos/apexLogo.fw.png" width = 40px onClick="mobileMenu();"></li></ul>');
+				document.getElementById("chunk").setAttribute("onClick", "removeMobileMenu();");
 			}
 			/* this function can be used to change
 			any image on the site to another image
 			assuming that the image has an ID. */
-			function changeImage(id, img) {
-				document.getElementById(id).setAttribute("src", img);
+			function changeSrc(id, value) {
+				document.getElementById(id).setAttribute("src", value);
 			}
 			/* this function can be used to change the page's stylesheet */
 			function changeStyle(sheet) {
 				document.getElementById("pageStyle").setAttribute("href", sheet);
 			}
-			function getId(id) {
-				document.getElementById(id);
-			}
-			function innHTML(value) {
-				innerHTML = value;
-			}
+			/* this function makes innerhtml to element by id easier */
 			function writeTo(id, value) {
-				getId("menu").innHTML(value);
+				document.getElementById(id).innerHTML = value;
 			}
-
+			/* This function causes the mobile friendly nav bar to pop up */
 			function mobileMenu() {
 				document.getElementById("menu").innerHTML = '<ul onClick="removeMobileMenu();"><li id = "logoContainer"><img id = "logoImage" src ="Photos/apexLogo.fw.png" width = 40px></li><li><a href="#intro" class="menuLink">Home</a></li><li><a href="#about" class="menuLink">About</a></li><li><a href="#pricing" class="menuLink">Pricing</a></li><li><a href="#contact" class="menuLink">Contact</a></li><li onClick="convertMobile();"><a class="menuLink">Mobile</a></li></ul>';
 			}
+			/* This function causes the mobile friendly nav bar to close */
 			function removeMobileMenu() {
 				document.getElementById("menu").innerHTML = '<ul><li id = "logoContainer"><img id = "logoImage" src ="Photos/apexLogo.fw.png" width = 40px onClick="mobileMenu();"></li></ul>';
 			}
@@ -46,7 +42,7 @@
  				if( navigator.userAgent.match(/Android/i)
  					|| navigator.userAgent.match(/webOS/i)
 					|| navigator.userAgent.match(/iPhone/i)
- 					|| navigator.userAgent.match(/iPad/i)
+ 					//|| navigator.userAgent.match(/iPad/i)
  					|| navigator.userAgent.match(/iPod/i)
  					|| navigator.userAgent.match(/BlackBerry/i)
  					|| navigator.userAgent.match(/Windows Phone/i)
@@ -56,4 +52,16 @@
  				else {
     				changeStyle("default.css");
   				}
+			}
+			/* this function will submit the form, and check
+			if the client is human. */
+			function submitForm() {
+				var question = "If I asked you what your name is, and it was Jacob, what would your name be? (To check you aren't a robot)";
+				var answer = "jacob";
+				if (prompt(question).toLowerCase() == answer) {
+					alert("yay");
+				}
+				else {
+					alert("You are not human!");
+				}
 			}
